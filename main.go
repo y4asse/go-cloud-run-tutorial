735806215929b3d100a6e5f35e3cab2aca68cfa3
y4asse <yasse0218@icloud.com>
2023-05-27 13:37:48 +0900

    first commit

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func main() {
 func handler(w http.ResponseWriter, r *http.Request) {
 	name := os.Getenv("NAME")
 	if name == "" {
-		name = "World"
+		name = "World!"
 	}
 	fmt.Fprintf(w, "Hello %s!\n", name)
 }
